d9d7c182719a39b95e0cbe9eb3dbe81bffa1485b
10taha92 <tasneembasra92@gmail.com>
2021-09-02 23:06:08 +0530

    oauth repaired

--- app.js
+++ app.js
@@ -68,13 +68,13 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: `http://localhost:${port}/auth/google/secrets`,
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
+    console.log(profile.displayName);
 
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    User.findOrCreate({username:profile.displayName, googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
   }
